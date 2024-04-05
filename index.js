7142360f41c2f4cd7fd8d30cebf59f949f8d323a
Mattrf16 <138604828+Mattrf16@users.noreply.github.com>
2024-04-05 10:34:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ register = document.getElementById("register");
 login = document.getElementById("login");
 loginPage = document.querySelector(".login");
 registerPage = document.querySelector(".register-box");
+bottone = document.querySelector(Accedi);
+password = document.getElementById("password");
+email = document.getElementById("email");
+passwordRegistrazione = document.getElementById("passwordRegistrazione");
+emailRegistrazione = document.getElementById("emailRegistrazione");
+
+
+bottone.addEventListener("click", ()=>{
+    console.log(email.value);
+    console.log(password.value);
+})
 
 register.addEventListener("click", ()=>{
     loginPage.classList.add("active");
